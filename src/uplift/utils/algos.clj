693f7eb5c11f466ec9a28873ba2f8798fdca65a9
Sean Toner <stoner@redhat.com>
2015-10-07 22:41:19 -0400

    simple lazy map-like function

--- src/uplift/utils/algos.clj
+++ src/uplift/utils/algos.clj
@@ -1,1 +1,16 @@
 (ns uplift.utils.algos)
+
+(defn lmap
+  "Sometimes you don't want to iterate through every item in a sequence
+  This function will walk through the sequence applying f to each item
+  and then applying checkfn to the result of f.  If/when checkfn returns
+  a falsey value iteration will stop"
+  [f checkfn seq]
+  (lazy-seq
+    (let [h (first seq)
+          t (rest seq)
+          result (f h)
+          continue (checkfn result)]
+      (if (and continue (not-empty t))
+        (cons result (lmap f checkfn t))
+        nil))))
